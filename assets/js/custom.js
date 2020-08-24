263490eaef98efcf53ba17dcc631bdc00bf64353
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-08-24 15:12:04 -0400

    worked on alignments

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".navbar").css("background" , "rgba(0, 0, 0, 0.9)");
+        }
+  
+        else{
+            $(".navbar").css("background" , "transparent");  	
+        }
+    })
+  })
+
+
+  // masonry 
